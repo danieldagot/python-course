9fd813b7dd0c82e7db04068c9a120b7f017f63a3
unknown <omer.moradd@gmail.com>
2019-02-24 15:31:44 +0200

    init

---
+++ ex1.py
@@ -0,0 +1,21 @@
+import turtle
+
+turtle.title("303083414As02")
+turtle.hideturtle()
+turtle.speed(5)
+turtle.begin_fill()
+turtle.pensize(3)
+turtle.pencolor('yellow')
+turtle.fillcolor('pink')
+turtle.setup(width=510, height=430)
+
+turtle.forward(175)
+turtle.left(120)
+turtle.forward(175)
+turtle.left(120)
+turtle.forward(175)
+turtle.left(120)
+
+turtle.end_fill()
+turtle.bgcolor("green")
+turtle.mainloop()
