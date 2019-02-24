8af9a223f678a6e407a0fb7cdffd34978938dc34
unknown <omer.moradd@gmail.com>
2019-02-24 20:49:56 +0200

    Set the position of the text

--- ex1.py
+++ ex1.py
@@ -1,14 +1,16 @@
 import turtle
 
+# Set all the different colors anf dimensions
 turtle.title('303083414As02')
 turtle.hideturtle()
+turtle.setup(width=510, height=430)
 turtle.speed(5)
 turtle.begin_fill()
 turtle.pensize(3)
 turtle.pencolor('yellow')
 turtle.fillcolor('pink')
-turtle.setup(width=510, height=430)
 
+# Draw the triangle
 turtle.forward(175)
 turtle.left(120)
 turtle.forward(175)
@@ -19,8 +21,12 @@ turtle.left(120)
 turtle.end_fill()
 
 turtle.bgcolor('green')
-turtle.pencolor('black')
 
+# Set text
+turtle.pencolor('black')
+turtle.penup()
+turtle.goto(-85, 180)
+turtle.pendown()
 turtle.write('Omer Morad - 303083414', align='center', font=('Arial', 20, 'bold'))
 
 turtle.mainloop()
