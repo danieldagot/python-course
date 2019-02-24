2d0ed0652ac1a199cabed40f20dcfe047e021538
unknown <omer.moradd@gmail.com>
2019-02-24 15:46:41 +0200

    Added text

--- ex1.py
+++ ex1.py
@@ -1,6 +1,6 @@
 import turtle
 
-turtle.title("303083414As02")
+turtle.title('303083414As02')
 turtle.hideturtle()
 turtle.speed(5)
 turtle.begin_fill()
@@ -17,5 +17,10 @@ turtle.forward(175)
 turtle.left(120)
 
 turtle.end_fill()
-turtle.bgcolor("green")
+
+turtle.bgcolor('green')
+turtle.pencolor('black')
+
+turtle.write('Omer Morad - 303083414', align='center', font=('Arial', 20, 'bold'))
+
 turtle.mainloop()
